05ad0d780372f1e5c7210e0f1711ccee131747c3
Lines3 <sporsheim.line@gmail.com>
2025-11-24 22:46:24 +0100

    update to script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// import { CONFIG } from "./config.js";
+import { CONFIG } from "./config.js";
 const formRegister = document.querySelector(".btn-primary");
 console.log(formRegister);
 const loginErrorMessage = document.querySelector(".loginErrorMessage");
