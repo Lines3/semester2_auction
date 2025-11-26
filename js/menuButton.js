b87922d793aea8f5209d867f94741519ab0dc4bf
Lines3 <sporsheim.line@gmail.com>
2025-11-26 22:00:53 +0100

    updates to burger menu

---
+++ js/menuButton.js
@@ -0,0 +1,7 @@
+// const menuBtn = document.querySelector(".navbar-toggler");
+// const mobileMenu = document.querySelector(".mobile-menu");
+
+// menuBtn?.addEventListener("click", () => {
+//   mobileMenu?.classList.toggle("active");
+//   mobileMenu?.classList.toggle("hidden");
+// });
